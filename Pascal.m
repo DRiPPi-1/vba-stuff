9c9f045b51b193a3c0c2eaac4403bd7839f2c779
Hlumelo Siyo <hlumelosiyo2@gmail.com>
2023-03-07 18:12:11 +0200

    Modified and added new files
    Modified matlab code and excel file, and added txt, docx and pdf of problem statement

--- Pascal.m
+++ Pascal.m
@@ -1,19 +1,20 @@
 % pascal
 clear
-n=input("Enter value: ");
-sum = 1;
+n=input("Enter value: "); % input of nth coefficient
 
 for a=1:n+1    
-    fprintf("%s", " ");
+    fprintf("%s", " "); % print spaces before the first 1
 end
-fprintf('%d \n', 1);
+fprintf('%d \n', 1); % print the the 1 on top o ftriangle
+
+% print rest of the triangle
 for b=1:n
     for c=n:-1:b
-        fprintf("%s", " ");
+        fprintf("%s", " ");  % print the spaces before each line
     end
-    fprintf('%d %d', 1, b);
+    fprintf('%d %d', 1, b); % print left side of the triangle i.e. 1 and n 
     for d=2:b-2
-        fprintf(' %d', b+1);
+        fprintf(' %d', b+1); % print number after 1 and n
     end
     fprintf(' %d %d \n',b, 1);
 end
